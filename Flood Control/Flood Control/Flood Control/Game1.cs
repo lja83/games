d3b1258cdf0ad1c143b99d5be0ec4e3678cc4887
Jeff Apczynski <jeff.apczynski@gmail.com>
2011-03-29 00:45:34 -0700

    Adding GamePiece class.  Stopped on page 35.

--- Flood Control/Flood Control/Flood Control/Game1.cs
+++ Flood Control/Flood Control/Flood Control/Game1.cs
@@ -19,6 +19,10 @@ namespace Flood_Control
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        Texture2D playingPieces;
+        Texture2D backgroundScreen;
+        Texture2D titleScreen;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -47,7 +51,9 @@ namespace Flood_Control
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            playingPieces = Content.Load<Texture2D>("Textures/Tile_Sheet");
+            backgroundScreen = Content.Load<Texture2D>("Textures/Background");
+            titleScreen = Content.Load<Texture2D>("Textures/TitleScreen");
         }
 
         /// <summary>
---
+++ Flood Control/Flood Control/Flood Control/GamePiece.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Flood_Control
+{
+    class GamePiece
+    {
+        public static string[] PieceTypes = 
+        {
+            "Left,Right",
+            "Top,Bottom",
+            "Left,Top",
+            "Top,Right",
+            "Right,Bottom",
+            "Bottom,Left",
+            "Empty"
+        };
+
+        public const int PieceHeight = 40;
+        public const int PieceWidth = 40;
+
+        public const int MaxPlayablePieceIndex = 5;
+        public const int EmptyPieceIndex = 6;
+
+        private const int textureOffsetX = 1;
+        private const int textureOffsetY = 1;
+        private const int texturePaddingX = 1;
+        private const int texturePaddingY = 1;
+
+        private string pieceType = "";
+        private string pieceSuffix = "";
+
+        public string PieceType
+        {
+            get { return pieceType; }
+        }
+
+        public string Suffix
+        {
+            get { return pieceSuffix; }
+        }
+
+        public GamePiece(string type, string suffix)
+        {
+            pieceType = type;
+            pieceSuffix = suffix;
+        }
+
+        public GamePiece(string type)
+        {
+            pieceType = type;
+            pieceSuffix = "";
+        }
+
+        public void SetPiece(string type, string suffix)
+        {
+            pieceType = type;
+            pieceSuffix = suffix;
+        }
+
+        public void SetPiece(string type)
+        {
+            pieceType = type;
+            pieceSuffix = "";
+        }
+
+        public void AddSuffix(string suffix)
+        {
+            if (!pieceSuffix.Contains(suffix))
+                pieceSuffix += suffix;
+        }
+
+        public void RemoveSuffix(string suffix)
+        {
+            pieceSuffix = pieceSuffix.Replace(suffix, "");
+        }
+    }
+}
